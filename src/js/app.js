f33ba1a7fd0fe21fee32c36f8fdfa27a89a87cd2
Rodion <rodionmalkov@gmail.com>
2021-10-10 00:12:32 +0500

    09.10.2021
    add adaptive
    @media only screen and (min-device-width: 320px) and (max-device-width: 480px)

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ import "../index.html";
 import Swiper, { Pagination } from "swiper";
 import "../../node_modules/swiper/swiper-bundle.esm";
 
+// ---- SWIPER ----
+
 const swiper = new Swiper(".gently__slider", {
   loopFillGroupWithBlank: true,
 
@@ -16,4 +18,27 @@ const swiper = new Swiper(".gently__slider", {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+// ---- MODAL ERROR ----
+
+const modalPopup = document.querySelector(".app__popup");
+const modalTelephone = document.querySelector(".app__input");
+const modalBtnSend = document.querySelector(".app__button");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("input-phone");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+modalBtnSend.addEventListener("click", function (evt) {
+  if (!modalTelephone.value) {
+    modalPopup.classList.add("app__popup-error");
+  } else if (isStorageSupport) {
+    localStorage.setItem("input-phone", modalTelephone.value);
+  }
 });
